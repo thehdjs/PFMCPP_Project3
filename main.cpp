0f84b7b33001e7280f7a8aed73aa70e3bbc2e374
matkatmusic <matkatmusic@gmail.com>
2020-05-04 20:44:52 -0700

    added part3 instructions

--- main.cpp
+++ main.cpp
@@ -1,88 +1,66 @@
+/*
+ Project 3 - Part 3 / 5
+ video: Chapter 2 - Part 8
+ Constructors tasks
+
+ Create a branch named Part3
+
+ On a new branch:
+
+ 1) Add a constructor for each User-Defined-Type.
+ 
+ 2) amend some of your UDT's member functions to print out something interesting via std::cout
+ 
+ 3) Instantiate 1 or 2 instances of each of your user-defined types in the main function at the bottom of that file,
+
+ 4) call some of your UDT's amended member functions in main().
+ 
+ 5) add some std::cout statements in main() that print out your UDT's member variable values or values returned from your UDT member functions (if they return values)
+ 
+ After you finish defining each type/function:
+ click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ example:
+ */
+
 #include <iostream>
- /*
- Project 3 - Part 2 / 5
- Video: Chapter 2 Part 6
- Implementations tasks
- 
-Create a branch named Part2
+namespace Example 
+{
+struct UDT  // my user defined type named 'UDT'
+{
+    int a; //a member variable
+    UDT();              //1) the constructor
+    void printThing();  //the member function
+};
 
- tasks
- 0) delete all of the plain english pseudo-code you added in Part1.
-   - you should be left with only your UDTs.
+//the function definitions are outside of the class
+UDT::UDT()
+{
+    a = 0;
+}
 
- 1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
-    don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
-    you should be able to deduce the return type of those functions based on their usage in Person::run()
-    You'll need to insert the Person struct from the video in the space below.
- */
+void UDT::printThing()
+{
+    std::cout << "UDT::printThing() " << a << std::endl;  //2) printing out something interesting
+}
 
-    struct Foot
-    {
-        void stepForward();
-        int stepSize();
-    };
-
-    void Foot::stepForward()
-    {
-        // I have no clue what to put in here
-    }
-
-    int Foot::stepSize()
-    {
-        // I have no clue what to put in here
-        return 4;
-    }
-
-    struct Person
-    {
-        int age;
-        int height;
-        float hairLength;
-        float GPA;
-        unsigned int SATScore;
-        int distanceTraveled;
-        
-        Foot leftFoot;
-        Foot rightFoot;
-
-        void run(int howFast, bool startWithLeftFoot);
-
-    };
-
-    void Person::run(int howFast, bool startWithLeftFoot)
-    {
-        if (startWithLeftFoot == true)
-        {
-           leftFoot.stepForward();
-           rightFoot.stepForward();
-        }
-        else
-        {
-            rightFoot.stepForward();
-            leftFoot.stepForward();
-        }
+int main()
+{
+    UDT foo;              //3) instantiating a UDT named 'foo' in main()
+    foo.printThing();     //4) calling a member function of the UDT instance.
     
-        distanceTraveled += leftFoot.stepSize() + rightFoot.stepSize();
+    //5) a std::cout statement accessing foo's member variable.
+    //It also demonstrates a 'ternary expression', which is syntactic shorthand for an 'if/else' expression
+    std::cout << "Is foo's member var 'a' equal to 0? " << (foo.a == 0 ? "Yes" : "No") << "\n";
+    
+    return 0;
+}
+} //end namespace Example
 
-        howFast = 1;
+//insert Example::main() into main() of user's repo.
 
-    }
 
- /*
- 2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
- 
- 3) be sure to write the correct full qualified name for the nested type's member functions.
- 
- 4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
- 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
-send me a DM to check your pull request
-
- Wait for my code review.
- */
 
 
 struct Knife
@@ -493,5 +471,6 @@ float Synthesizer::makeDrums()
 
 int main()
 {
+    Example::main();
     std::cout << "good to go!" << std::endl;
 }
